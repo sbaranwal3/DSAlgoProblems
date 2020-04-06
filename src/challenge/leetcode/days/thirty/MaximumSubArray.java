8c21e0e3997094f1279dfa11e516d7d807ef5ac2
Shubham Baranwal <shubham.baranwal@target.com>
2020-04-06 09:01:19 +0530

    Updated details and added day5 challenge solution

--- src/challenge/leetcode/days/thirty/MaximumSubArray.java
+++ src/challenge/leetcode/days/thirty/MaximumSubArray.java
@@ -2,6 +2,19 @@ package challenge.leetcode.days.thirty;
 
 public class MaximumSubArray {
 
+    /*
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+     */
+
         public static int maxSubArray(int[] nums) {
             int maxSoFar = nums[0];
             int maxEndingHere =  nums[0];
@@ -9,15 +22,12 @@ public class MaximumSubArray {
                 maxEndingHere += nums[i];
                 maxEndingHere = Math.max(nums[i], maxEndingHere);
                 maxSoFar = Math.max(maxEndingHere,maxSoFar);
-//                if(maxEndingHere > maxSoFar){
-//                    maxSoFar = maxEndingHere;
-//                }
             }
             return maxSoFar;
         }
 
     public static void main(String[] args) {
-            int nums[] = {1};
+            int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println("Largest sum of contiguous array is "+maxSubArray(nums));
 
     }
