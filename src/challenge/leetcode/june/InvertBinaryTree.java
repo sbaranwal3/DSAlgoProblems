29e8f42c9dbddeb9942759ed83d141c599194cf5
Shubham Baranwal <shubham.baranwal@target.com>
2020-06-02 07:58:16 +0530

    Adding solution for LeetCode June challenge day1

---
+++ src/challenge/leetcode/june/InvertBinaryTree.java
@@ -0,0 +1,83 @@
+package challenge.leetcode.june;
+
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+ */
+public class InvertBinaryTree {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return root;
+        TreeNode temp;
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        System.out.println("Binary tree before inversion is: ");
+        printInorder(root);
+        System.out.println("Binary tree after inversion is: ");
+        printInorder(invertTree(root));
+
+    }
+
+    static void printInorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        printInorder(node.left);
+        printInorder(node.right);
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
